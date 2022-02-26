9dfa437c9c6f39a0e800dbf2f6b46366e743b559
ThibaultPaprocki <alex.paprocki@gmail.com>
2022-02-26 14:20:52 +0100

    parser works for one code

--- cleanCode/src/main/java/Parser.java
+++ cleanCode/src/main/java/Parser.java
@@ -1,81 +1,105 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Parser {
 
-    final private Map<String,Integer> code = Map.of("000010010",1,
-                                                    "020021120",2,
-                                                    "020021021",3,
-                                                    "000121001",4,
-                                                    "020120021",5,
-                                                    "020120121",6,
-                                                    "020001001",7,
-                                                    "020121121",8,
-                                                    "020121021",9);
+    final private Map<String,Integer> codes = Map.of("020101121",0,
+            "000010010",1,
+            "020021120",2,
+            "020021021",3,
+            "000121001",4,
+            "020120021",5,
+            "020120121",6,
+            "020001001",7,
+            "020121121",8,
+            "020121021",9);
 
 
 
-
-
-    public void parseFile(File file) throws IOException {
+    public void parseFile(File file) throws Exception {
 
         FileReader fileReader = new FileReader(file);
         BufferedReader br = new BufferedReader(fileReader);
-        List<String> numbersCode = new ArrayList<>();
-
-        String line = "";
-        int countLine = 1;
+        String[] numbersCode = new String[100];
+        Arrays.fill(numbersCode, "");
+        String[] resultCode = new String[9];
+        Arrays.fill(resultCode, "");
+        String line;
+        int countLine = 0;
         int characWidth = 0;
+        int[] number = new int[9];
+        Arrays.fill(resultCode, "");
+        String character;
 
         while((line = br.readLine()) != null){
-            System.out.print("aa");
-            if(countLine==4) {
-                displayNumber(getCodeValue(numbersCode));
-                countLine = 1;
-                characWidth=0;
-            }
-            else {
 
-                for (int i = 0; i < 27; i++) {
-                    System.out.print("bb");
-
-                    if (i % 3 != 0 || characWidth == 0) {
-                        characWidth++;
-                        numbersCode.add("" + line.charAt(i));
-                    }
-                    else {
-                        numbersCode.set(characWidth, numbersCode.get(characWidth) + "" + line.charAt(i));
-                    }
-                    System.out.print("cc");
-
-                }
-            }
+            numbersCode[countLine]=line;
+            numbersCode[countLine]=addMissingEscape(numbersCode[countLine]);
             countLine++;
+            if(countLine%4 == 0) {
+                countLine=0;
+            }
         }
 
-        System.out.println(numbersCode);
-        System.out.println(line);
+
+        for (int i = 0; i < countLine; i++) {
+            System.out.println(i+" : "+countLine);
+
+            for (int j = 0; j < numbersCode[i].length(); j++) {
+                if(j == 3  j == 6  j == 9  j == 12  j == 15  j == 18  j == 21  j == 24  j == 27 ){
+                    characWidth++;
+                }
+                character=getCharacterCode(numbersCode[i].charAt(j));
+                resultCode[characWidth]+=character;
+            }
+            characWidth = 0;
+
+            if(i==2 || i==5){
+                for (int j = 0; j < resultCode.length; j++) {
+                    number[j] = getCodeValue(resultCode[j]);
+                }
+                displayNumber(number);
+            }
+        }
         br.close();
     }
 
-    public ArrayList<Integer> getCodeValue(List<String> numberCodes){
-        ArrayList<Integer> numbers = new ArrayList<>();
-        for (String numberCode: numberCodes) {
-            for (Map.Entry<String, Integer> entry : code.entrySet()) {
-                if (entry.getValue().equals(numberCode)) {
-                    numbers.add(entry.getValue());
-                }
+
+    private String addMissingEscape(String s) {
+        if(s.length()!=27){
+            for (int i = s.length(); i < 27 ; i++) {
+                s+=" ";
             }
         }
-        return numbers;
+        return s;
     }
 
-    public void displayNumber(ArrayList<Integer> number){
-        for (int i = 0; i < number.size(); i++) {
-            System.out.print(number.get(i));
+    public int getCodeValue(String number) {
+        int code = 0;
+        for (Map.Entry<String, Integer> entry : codes.entrySet()) {
+            if (entry.getKey().equals(String.valueOf(number))) {  // ici c'est la key
+                code = entry.getValue();
+            }
         }
-        System.out.println();
+        return code;
+    }
+
+    public void displayNumber(int[] number){
+        for (int i = 0; i < number.length; i++) {
+            System.out.print(number[i]);
+        }
+    }
+
+    public String getCharacterCode(char c){
+        switch(c){
+            case ' ': return "0";
+            case '|': return "1";
+            case '_': return "2";
+            default: return "";
+        }
     }
 }
