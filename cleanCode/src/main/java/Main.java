63167d3fb29250396ebc0e42867d52fe5dcd9259
Alexandre Paprocki <alex.paprocki@gmail.com>
2022-03-03 13:41:05 +0100

    Full parser

--- cleanCode/src/main/java/Main.java
+++ cleanCode/src/main/java/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws Exception {
         Parser parser = new Parser();
-        File file = new File("C:\\Users\\lucas\\IdeaProjects\\cleanCode\\cleanCode\\src\\test\\ressources\\blbl");
+        File file = new File("cleanCode/src/test/ressources/blbl");
         parser.parseFile(file);
     }
 }
--- cleanCode/src/main/java/Parser.java
+++ cleanCode/src/main/java/Parser.java
@@ -1,12 +1,9 @@
 import java.io.*;
 import java.util.Arrays;
 import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class Parser {
-
+    private PrintWriter writer;
     final private Map<String,Integer> codes = Map.of("020101121",0,
             "000010010",1,
             "020021120",2,
@@ -17,7 +14,6 @@ public class Parser {
             "020001001",7,
             "020121121",8,
             "020121021",9);
-
 
 
     public void parseFile(File file) throws Exception {
@@ -32,23 +28,36 @@ public class Parser {
         int countLine = 0;
         int characWidth = 0;
         int[] number = new int[9];
-        Arrays.fill(resultCode, "");
         String character;
-        String checksumResult;
+        String checkSumResult;
+//        try {
+//            File myFile = new File("src/test/ressourcesNewFile.txt");
+//
+//            if (myFile.createNewFile()){
+//                System.out.println("Le fichier est créé.");
+//            }else{
+//                System.out.println("Le fichier existe déjà.");
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        //  writer = new PrintWriter("ressourcesNewFile.txt", "UTF-8");
 
         while((line = br.readLine()) != null){
+
             numbersCode[countLine]=line;
             numbersCode[countLine]=addMissingEscape(numbersCode[countLine]);
             countLine++;
-            if(countLine%4 == 0) {
-                countLine=0;
+            if (countLine%3==0){
+                br.readLine();
             }
-        }
 
+        }
 
         for (int i = 0; i < countLine; i++) {
             for (int j = 0; j < numbersCode[i].length(); j++) {
-                if(j == 3 || j == 6 || j == 9 || j == 12 || j == 15 || j == 18 || j == 21 || j == 24 || j == 27 ){
+                if(j%3 == 0 && j!=0){
                     characWidth++;
                 }
                 character=getCharacterCode(numbersCode[i].charAt(j));
@@ -56,14 +65,14 @@ public class Parser {
             }
             characWidth = 0;
 
-            if(i==2 || i==5){
+            if(i%3 == 2){
                 for (int j = 0; j < resultCode.length; j++) {
                     number[j] = getCodeValue(resultCode[j]);
                 }
-
-                checksumResult = checkChecksum(number);
-
-                displayResults(number,checksumResult);
+                checkSumResult = checkChecksum(number);
+                displayResults(number,checkSumResult);
+                Arrays.fill(resultCode, "");
+                Arrays.fill(number,0);
             }
         }
         br.close();
@@ -81,32 +90,39 @@ public class Parser {
 
     public int getCodeValue(String number) {
         int errorCode = 42;
-
         for (Map.Entry<String, Integer> entry : codes.entrySet()) {
-            if (entry.getKey().equals(String.valueOf(number))) {  // ici c'est la key
+            if (entry.getKey().equals(String.valueOf(number))) {
                 return entry.getValue();
             }
         }
         return errorCode;
     }
 
-    public void displayResults(int[] number, String checksumResult){
-        String errorMsg = "";
+    public void displayResults(int[] number, String checksumResult) throws FileNotFoundException, UnsupportedEncodingException {
 
+        String errorMsg = "";
         for (int i = 0; i < number.length; i++) {
             if(number[i] == 42){
                 System.out.print("?");
+                //writer.write("?");
                 errorMsg = " ILL";
             }
-            else System.out.print(number[i]);
+            else{
+                // writer.write(number[i]);
+                System.out.print(number[i]);
+            }
         }
 
         if(errorMsg == " ILL"){
+            //writer.print(errorMsg);
             System.out.print(errorMsg);
         }
         else{
+            //writer.print(checksumResult);
             System.out.print(checksumResult);
         }
+        //writer.println();
+        System.out.println();
 
     }
 
@@ -121,17 +137,16 @@ public class Parser {
 
     private String checkChecksum(int[] number){
         int totalChecksum = 0;
-        int position = 0;
+        int position = 1;
 
         for(int i = number.length; i > 0; i--){
             totalChecksum += (i*position);
             position ++;
         }
 
-        if(totalChecksum % 11 != 0){
+        if(totalChecksum % 11 != 0){ // SUSPICIEUX LUCAS  totalChecksum % 11 != 0 ??
             return " ERR";
         }
-
         return "";
     }
 }
