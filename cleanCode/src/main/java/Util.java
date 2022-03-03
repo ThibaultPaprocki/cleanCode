a59a3431a78010c82a876bd22fb5fdca5f18e9ae
Alexandre <alexandre.paprocki@inrae.fr>
2022-03-04 00:51:22 +0100

    menu rework + readme

--- cleanCode/src/main/java/Util.java
+++ cleanCode/src/main/java/Util.java
@@ -1,8 +1,9 @@
 import java.io.File;
+import java.util.List;
 
 public class Util {
 
-    public static void showHelp(){
+    public static void showHelp() {
         System.out.println("""
                 Welcome to the help terminal commands !\s
                 BEWARE !!! You need to write the pathfile in the input\s
@@ -11,29 +12,37 @@ public class Util {
 
                 -help             show all commands
                 pathfile.txt      the pathfile to the file to use
-                -classificate     returns three files / Authorized / Errored / Unknown with all the checksums
-                -result           returns a file 'result.txt that contains the results even unknown ones'
-                -checksum         returns a file 'checksum.txt' that contains the results with a checksum
+                -create           returns a file 'result.txt that contains the results even unknown ones'
                 \s
                 Thank you for your help we hope that we help you !!""");
     }
 
     public static void menu(Parser parser, String[] args) throws Exception {
-        File file = new File("cleanCode/src/test/ressources/blbl.txt");
-        for(String arg : args){
-            switch (arg) {
-                case ".txt":
+        boolean createFile = false;
+
+        List<File> files = null;
+        if (args.length <= 0) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < args.length; i++) {
+
+            switch (args[i]) {
+                case "-help":
+                    showHelp();
                     break;
-                case "-help": showHelp();
+                case "-create":
+                    createFile = true;
                     break;
-                case "-result":
-                    break;
-                case "-checksum":
-                    break;
-                case "-classificate":
-                    break;
-                default: throw new IllegalArgumentException();
+                default:
+                    if (args[i].endsWith(".txt")) {
+                        files.add(new File(args[i]));
+                    } else {
+                        throw new IllegalArgumentException();
+                    }
             }
+        }
+        for (File file: files) {
+            parser.parseFile(file,createFile);
         }
     }
 }
